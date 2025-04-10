1c1a15bf2b554c62ff1420d69527a611624d0956
Lindokuhle Nqwala <nqwala1994@gmail.com>
2025-04-10 19:17:03 +0200

    Add docstrings to forms.py

--- my-static-site/band_site/band/forms.py
+++ my-static-site/band_site/band/forms.py
@@ -3,6 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class UserRegistrationForm(UserCreationForm):
+    """
+    A custom form for registering new users, extending Django's UserCreationForm.
+
+    Includes additional fields such as email.
+    """
     email = forms.EmailField()
 
     class Meta:
