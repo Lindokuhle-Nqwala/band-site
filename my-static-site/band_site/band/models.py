a2556ee8d0f8e1547f1bb3913586d319ec1e49ee
Lindokuhle Nqwala <nqwala1994@gmail.com>
2025-04-10 19:17:05 +0200

    Add docstrings to models.py

--- my-static-site/band_site/band/models.py
+++ my-static-site/band_site/band/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
-# Create your models here.
 class Concert(models.Model):
+    """
+    Model representing a concert event.
+
+    Attributes:
+        name (str): Name of the concert.
+        date (datetime): Date and time of the concert.
+        venue (str): Venue where the concert will be held.
+        description (str): Description of the concert.
+    """
     name = models.CharField(max_length=200)
     date = models.DateTimeField()
     venue = models.CharField(max_length=200)
     description = models.TextField()
 
     def __str__(self):
-         return f"{self.title} @ {self.venue} on {self.date}"
+        """
+        Returns a readable string representation of the concert.
+        """
+        return f"{self.name} @ {self.venue} on {self.date}"
