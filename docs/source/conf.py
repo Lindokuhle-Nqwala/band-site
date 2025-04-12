1e4b7d66ce7d1ca1033c8d397a2288eb1b9829d4
Lindokuhle Nqwala <111274350+Lindokuhle-Nqwala@users.noreply.github.com>
2025-04-12 15:35:29 +0200

    Add files via upload

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,34 +6,25 @@ sys.path.insert(0, os.path.abspath('../../my-static-site/band_site'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'band_site.settings'
 django.setup()
 
-
-
-
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Band Site'
 copyright = '2025, Lindokuhle Nqwala'
 author = 'Lindokuhle Nqwala'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'alabaster'
 html_static_path = ['_static']
